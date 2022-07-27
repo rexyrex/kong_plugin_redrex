3a2ec63d1e20b14c3642f4c14a1db3f034da3bff
민형 김 <rexyrex@nexon.co.kr>
2022-07-27 13:52:03 +0900

    d

--- kong/plugins/redrex/schema.lua
+++ kong/plugins/redrex/schema.lua
@@ -3,13 +3,6 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
     no_consumer = true, -- This means our plugin will not apply to specific service consumers
     fields = {		  
-		validate_token_url = typedefs.url({ required = true }),
-		
-		uri_param_names = {
-			  type = "set",
-			  elements = { type = "string" },
-			  default = { "access_token" },
-			},
 		
         status_code = {
             type     = "number",
